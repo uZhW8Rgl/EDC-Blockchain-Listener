f0efe88c004f1f8baa78169a2f026992342cb21f
uZhW8Rgl <sonak53@googlemail.com>
2024-06-11 13:53:34 +0200

    feat: fetch and forward verifiable presentation to FC

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import "./taquito/listener.js";
+//import {getToken} from "./taquito/listener.js";
 
 import { createRequire } from "module";
+//import cors from 'cors';
 const require = createRequire(import.meta.url);
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 
+//export 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
@@ -14,6 +17,7 @@ const port = 3005;
 
 
 app.use(express.json());
+//app.use(cors());
 
 app.post('/webhook', (req, res) => {
     console.log('Webhook received:', req.body);
@@ -25,4 +29,45 @@ app.post('/webhook', (req, res) => {
     res.sendStatus(200);
 });
 
+
+
+
 server.listen(port, () => console.log('Server listening on port ' + port + '.'));
+
+const axios = require('axios');
+const qs = require('qs');
+
+export let access_token = '';
+export let refresh_token = '';
+
+let data = qs.stringify({
+    'grant_type': 'password',
+    'username': process.env.keycloak_user_username,
+    'password': process.env.keycloak_user_password,
+    'client_id': process.env.client_id,
+    'scope': 'openid',
+    'client_secret': process.env.client_secret 
+  });    
+let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: process.env.keycloak_protocol +'://'+ process.env.keycloak_address + '/realms/gaia-x/protocol/openid-connect/token',
+    headers: { 
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+      data : data
+};
+    
+axios.request(config)
+.then((response) => {
+    access_token = response.data.access_token;
+    refresh_token = response.data.refresh_token;
+    //console.log(JSON.stringify(response.data, null, 2));
+    console.log(access_token);
+    //console.log(refresh_token);
+    //getToken(contractConfig.contractAddress,token_int_position)
+})
+.catch((error) => {
+    console.log(error);
+});
+
